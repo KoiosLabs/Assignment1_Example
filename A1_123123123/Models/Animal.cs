fd46f22389cf69ce746ac3bc76c8d15d4149000a
Justin Hunt <Justin.hunt@georgiancollege.ca>
2019-11-06 16:18:00 -0500

    Add project files.

---
+++ A1_123123123/Models/Animal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A1_123123123.Models
+{
+    public class Animal
+    {
+        public virtual int Id { get; set; }
+        public virtual String Name { get; set; }
+        public virtual String Description { get; set; }
+        public virtual List<PetFood> PetFoods { get; set; }
+    }
+}
---
+++ A1_123123123/Models/PetFood.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A1_123123123.Models
+{
+    public class PetFood
+    {
+        public virtual int Id { get; set; }
+
+        public virtual Decimal Price { get; set; }
+        public virtual String Name { get; set; }
+        public virtual String Description { get; set; }
+        public virtual String NutritionalInfo { get; set; }
+        public virtual float Weight { get; set; }
+        public virtual String Brand { get; set; }
+        public virtual Animal Animal { get; set; }
+        public virtual int AnimalId { get; set; }
+    }
+}
